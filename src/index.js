413c6048dce1ffda8199e937028032e92129c1b8
vishal9334 <vishukr59@gmail.com>
2025-06-25 14:08:23 +0530

    Utils code on disscussion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/db.js";
-
+import {app} from "./app.js"
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, ()=> console.log(`server is running at PORT ${process.env.PORT}`),
+    console.log("mongoDB connected successfull!"))
+  })
+  .catch((err) => console.log("mongoDB connection failed !!!", err));
 
 
 
@@ -14,7 +19,11 @@ connectDB()
 
 
 
-// Aproch first of connect with database
+
+
+
+
+  // Aproch first of connect with database
 /*
 import express from "express";
 const app = express()(async () => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler =(fn)=> ()=>{}
+// const asyncHandler =(fn)=> async ()=>{}
+
+// Approch 1
+// const asyncHandler = (fn)=> async (req, res, next)=>{
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 5000).json({
+//             success: false,
+//             message:`${error.message}`
+
+//         })
+//     }
+// }
