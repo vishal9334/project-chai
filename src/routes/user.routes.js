b906d2877ec4f077f8293fdc17b893bdf95865e8
vishal9334 <vishukr59@gmail.com>
2025-08-24 23:43:04 +0530

    feat: add routes for changing password, fetching current user, and updating account details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,9 +6,11 @@ import {
   logoutUser,
   refreshAccessToken,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  changeCurrentPassword
 } from "../controller/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middelware.js";
+
 
 const router = Router();
 router.route("/register").post(
@@ -31,6 +33,12 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router.route("/update-avatar").patch( verifyJWT, upload.single("avatar"),updateUserAvatar)
 router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router;
